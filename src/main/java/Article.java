bb3b8a9704496b0a1ea7adc2bde6a1b916017b20
Paula Gómez <paulagomezmatos@gmail.com>
2023-04-04 13:29:38 +0200

    feat: implement JSON reader
    It contains a JSON reader class that returns a list of Articles, an
    Article class that represents each article input and a Main class
    where the JSON reader will interact with the Indexer.

---
+++ src/main/java/Article.java
@@ -0,0 +1,89 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import java.util.List;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Article {
+    private String id;
+    private String articleTitle;
+    private String abstractText;
+    private List<String> language;
+    private String pubDate;
+    private List<String> filterSources;
+    private List<String> keywords;
+    private List<String> pubTypes;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getArticleTitle() {
+        return articleTitle;
+    }
+
+    public void setArticleTitle(String title) {
+        this.articleTitle = title;
+    }
+
+    public String getAbstractText() {
+        return abstractText;
+    }
+
+    public void setAbstractText(String abstractText) {
+        this.abstractText = abstractText;
+    }
+
+    public List<String> getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(List<String> language) {
+        this.language = language;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public List<String> getFilterSources() {
+        return filterSources;
+    }
+
+    public void setFilterSources(List<String> filterResources) {
+        this.filterSources = filterResources;
+    }
+
+    public List<String> getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    public List<String> getPubTypes() {
+        return pubTypes;
+    }
+
+    public void setPubTypes(List<String> pubTypes) {
+        this.pubTypes = pubTypes;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+}
