0607f4b1abf6663b97ca2cb95e67c39b96a67d98
paulanormal_activity <60174977+PaulokaxD@users.noreply.github.com>
2023-04-18 09:49:27 +0200

    SPRINT 2: Index data to OpenSearch (#10)
    * feat: index articles documents

---
+++ src/main/java/ArticlesIndexer.java
@@ -0,0 +1,47 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHost;
+import org.opensearch.action.bulk.BulkRequest;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ArticlesIndexer {
+
+    private final RestHighLevelClient client;
+
+    public ArticlesIndexer(String host, Integer port, String scheme) {
+        this.client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost(host, port, scheme)));
+    }
+
+    public void bulkAppendArticles(String index, List<Article> articles) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        for (Article article : articles) {
+            BulkRequest request = new BulkRequest();
+            String jsonArticle = mapper.writeValueAsString(article);
+
+            IndexRequest indexRequest = new IndexRequest(index)
+                    .source(jsonArticle, XContentType.JSON);
+
+            request.add(indexRequest);
+            client.bulk(request, RequestOptions.DEFAULT);
+            // TODO: Add logguer: "Number of failed requests: " + bulkResponse.hasFailures()
+
+        }
+
+    }
+
+    public void close() {
+        try{
+            client.close();
+        }catch (IOException e){
+            System.out.println(e.getStackTrace());
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,53 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    private static final Integer N_INDEXING_FILES = 100;
+    private static final String INDEX_NAME = "articles";
+
+    public static void main(String[] args) {
         Path folderPath = Paths.get("data");
-        List<Path> pathStream = Files.walk(folderPath)
-                .filter(Files::isRegularFile)
-                .filter(path -> path.toString().endsWith(".json"))
-                .collect(Collectors.toList());
-        for(Path path : pathStream) {
-            List<Article> articles = JSONReader.readFile(path);
+        List<Path> paths = new ArrayList<>();
+
+        try (Stream<Path> pathStream = Files.walk(folderPath)) {
+            paths = pathStream
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".json"))
+                    .collect(Collectors.toList());
+        } catch (IOException e){
+            System.out.println(e.getStackTrace());
         }
+
+        ArticlesIndexer indexer = new ArticlesIndexer("localhost", 9200, "http");
+        int nFile = 1;
+        try {
+            for(Path path : paths) {
+                System.out.println("Processing file " + path.toString());
+
+                int fileNumber = JSONReader.getFileNumber(path);
+                List<List<Article>> articlesBatches = JSONReader.readFileIntoPartition(path, N_INDEXING_FILES);
+
+                Integer nBatch = 1;
+                for(List<Article> articlesBatch : articlesBatches){
+                    indexer.bulkAppendArticles(INDEX_NAME, articlesBatch);
+                    System.out.println("File "+nFile+"/"+paths.size()+" -> "+
+                            Math.min(N_INDEXING_FILES*nBatch,fileNumber)+"/"+fileNumber);
+                    nBatch++;
+                }
+                nFile++;
+            }
+        } catch (IOException e){
+            System.out.println(e.getStackTrace());
+        }finally {
+            indexer.close();
+        }
+
     }
 
 }
