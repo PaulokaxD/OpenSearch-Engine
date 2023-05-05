b6612848cc2cd26280cf0ac907b0a91f94e257d7
Paula Gómez <paulagomezmatos@gmail.com>
2023-05-05 09:44:19 +0200

    feat: Main checkstyle fixed

--- src/main/java/os/exercise/app/Main.java
+++ src/main/java/os/exercise/app/Main.java
@@ -6,9 +6,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
-import os.exercise.io.JSONReader;
-import os.exercise.opensearch.ArticlesIndexer;
-import os.exercise.models.Article;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -19,19 +16,39 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import os.exercise.io.JsonReader;
+import os.exercise.models.Article;
+import os.exercise.opensearch.ArticlesIndexer;
+
+/**
+ * Main class where all the funcionalities meets.
+ */
 public class Main {
 
     private static final Integer N_INDEXING_FILES = 100;
     private static final String INDEX_NAME = "articles";
-    private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
     private static final String DATA_PATH = "data";
 
+    private static final int PORT = 9200;
+    private static final String HOST = "localhost";
+    private static final String SCHEME = "http";
+
+    private Main() {
+    }
+
+    /**
+     * Main function where all the funcionalities meet to index the content of a folder with jsons containing articles
+     * into an OpenSearch index.
+     *
+     * @param args not used yet.
+     */
     public static void main(String[] args) {
         Path folderPath = Paths.get(DATA_PATH);
         List<Path> paths = getPaths(folderPath);
 
         RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(new HttpHost("localhost", 9200, "http")));
+                RestClient.builder(new HttpHost(HOST, PORT, SCHEME)));
 
         ObjectMapper mapper = new ObjectMapper();
         ArticlesIndexer indexer = new ArticlesIndexer(client, mapper);
@@ -39,18 +56,24 @@ public class Main {
         indexFile(paths, indexer);
     }
 
+    /**
+     * Index the articles in the json files using the {@ArticlesIndexer}.
+     *
+     * @param paths A list of json files.
+     * @param indexer An {@ArticlesIndexer}.
+     */
     public static void indexFile(List<Path> paths, ArticlesIndexer indexer) {
         int nFile = 1;
         for(Path path : paths) {
-            logger.info("Processing file {}", path);
+            LOGGER.info("Processing file {}", path);
 
-            int fileNumber = JSONReader.getFileNumber(path);
-            List<List<Article>> articlesBatches = JSONReader.readArticlesFileIntoPartition(path, N_INDEXING_FILES);
+            int fileNumber = JsonReader.getFileNumber(path);
+            List<List<Article>> articlesBatches = JsonReader.readArticlesFileIntoPartition(path, N_INDEXING_FILES);
 
             Integer nBatch = 1;
             for(List<Article> articlesBatch : articlesBatches){
                 indexer.bulkAppendArticles(INDEX_NAME, articlesBatch);
-                logger.info("File {}/{} -> {}/{},", nFile, paths.size(),
+                LOGGER.info("File {}/{} -> {}/{},", nFile, paths.size(),
                         Math.min(N_INDEXING_FILES*nBatch, fileNumber), fileNumber);
                 nBatch++;
             }
@@ -58,6 +81,12 @@ public class Main {
         }
     }
 
+    /**
+     * Get all json files path in a folder.
+     *
+     * @param folderPath The data folder.
+     * @return A list of all json paths.
+     */
     public static List<Path> getPaths(Path folderPath) {
         List<Path> paths = new ArrayList<>();
 
@@ -67,7 +96,7 @@ public class Main {
                     .filter(path -> path.toString().endsWith(".json"))
                     .collect(Collectors.toList());
         } catch (IOException e){
-            e.printStackTrace();
+            LOGGER.warn(e);
         }
         return paths;
     }
