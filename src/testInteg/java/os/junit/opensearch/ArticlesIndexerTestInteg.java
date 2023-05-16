0f972fae211c518cd4cc7e5f33b2bde241ce259e
Paula Gómez <paulagomezmatos@gmail.com>
2023-05-16 12:57:36 +0200

    feat: Two tests implemented WIP

--- src/testInteg/java/os/junit/opensearch/ArticlesIndexerTestInteg.java
+++ src/testInteg/java/os/junit/opensearch/ArticlesIndexerTestInteg.java
@@ -1,9 +1,15 @@
 package os.junit.opensearch;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.apache.http.HttpHost;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -41,14 +47,16 @@ class ArticlesIndexerTestInteg {
     private String hostname;
     private String scheme;
     private Integer port;
+    private String indexURL;
+    private String searchURL;
 
     private Article article1;
     private Article article2;
 
+    private ObjectMapper mapper;
+
     @BeforeAll
-    void setUp(){
-
-
+    void setUp() throws UnirestException {
         articlesPath = Paths.get("src\\test\\resources", "articles-example.json");
 
         article1 = new Article();
@@ -59,35 +67,53 @@ class ArticlesIndexerTestInteg {
         osContainer.start();
         hostname = osContainer.getHost();
         port = osContainer.getFirstMappedPort();
+        indexURL = String.format("http://%s:%d/%s", hostname, port, index);
+        searchURL = String.format("%s/_search", indexURL);
+
+        mapper = new ObjectMapper();
+
+        Unirest.setTimeouts(60000, 60000);
+        String mappingsQuery = String.join("",
+                "{\"settings\":{\"index\":{\"number_of_replicas\":0},",
+                "\"analysis\":{\"filter\":{\"english_stop\":{\"type\":\"stop\",",
+                "\"stopwords\":\"_english_\"},",
+                "\"english_stemmer\":{\"type\":\"stemmer\",",
+                "\"language\":\"english\"},",
+                "\"english_possessive_stemmer\":{\"type\":\"stemmer\",",
+                "\"language\":\"possessive_english\"},",
+                "\"lang_synonym\":{\"type\":\"synonym\",",
+                "\"synonyms\":[\"eng => English\"]}},",
+                "\"analyzer\":{\"english_analyzer\":{\"tokenizer\":\"standard\",",
+                "\"filter\":[\"english_possessive_stemmer\",\"lowercase\",",
+                "\"english_stop\",\"english_stemmer\"]},",
+                "\"autocomplete\":{\"tokenizer\":\"autocomplete\"},",
+                "\"keyword_analyzer\":{\"tokenizer\":\"keyword\",",
+                "\"filter\":[\"lowercase\"]},",
+                "\"lang_expansion\":{\"tokenizer\":\"keyword\",",
+                "\"filter\":[\"lang_synonym\"]}},",
+                "\"tokenizer\":{\"autocomplete\":{\"type\":\"edge_ngram\",",
+                "\"min_gram\":1,\"max_gram\":9,\"token_chars\":[\"digit\"]}}}},",
+                "\"mappings\":{\"properties\":{\"id\":{\"type\":\"text\",",
+                "\"analyzer\":\"autocomplete\"},",
+                "\"article_title\":{\"type\":\"text\",",
+                "\"analyzer\":\"english_analyzer\"},",
+                "\"abstract_text\":{\"type\":\"text\",",
+                "\"analyzer\":\"english_analyzer\"},",
+                "\"keywords\":{\"type\":\"text\",",
+                "\"analyzer\":\"keyword_analyzer\",\"fielddata\":\"true\"},",
+                "\"pub_types\":{\"type\":\"keyword\"},",
+                "\"language\":{\"type\":\"text\",",
+                "\"analyzer\":\"lang_expansion\",\"fielddata\":\"true\"},",
+                "\"pub_date\":{\"type\":\"date\"}}}}");
+
+        Unirest.put(indexURL)
+                .header("Content-Type", "application/json")
+                .body(mappingsQuery)
+                .asString();
     }
 
-//    @ClassRule
-//    public final ExternalResource externalResource = new ExternalResource() {
-//        @Override
-//        protected void before()  {
-//            osContainer.start();
-//            hostname = osContainer.getHost();
-//            port = osContainer.getFirstMappedPort();
-//
-////                try (RestClient client = RestClient
-////                        .builder(HttpHost.create(os_container.getHttpHostAddress()))
-////                        .build()) {
-////
-////                    final Response response = client.performRequest(new Request("GET", "/_cluster/health"));
-////                    ...
-////                }
-//        }
-//
-//        @Override
-//        protected void after() {
-//            osContainer.close();
-//        }
-//    };
-
     @Test
-    void givenCorrectJSONWhenIndexArticlesThenSuccess() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-
+    void givenCorrectJSONWhenIndexArticlesThenSuccess() throws IOException, UnirestException {
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(new HttpHost(hostname, port, scheme)));
 
@@ -97,13 +123,16 @@ class ArticlesIndexerTestInteg {
         BulkRequest request = indexer.getBulkRequest(index, articles);
 
         assertTrue(indexer.indexArticles(articles, request));
-        // TODO: HTTP REQUEST TO SEE NUMBER OF FILES
+
+        int hits = getHits();
+
+        assertEquals(articles.size(),hits);
+
+        clearIndex();
     }
 
     @Test
     void givenWrongJSONWhenIndexArticlesThenFailure() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(new HttpHost(hostname, port, scheme)));
 //      Throw(new IOException());
@@ -116,9 +145,7 @@ class ArticlesIndexerTestInteg {
     }
 
     @Test
-    void givenWrongArticleWhenIndexArticlesThenSuccess() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-
+    void givenWrongArticleWhenIndexArticlesThenSuccess() throws IOException, UnirestException {
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(new HttpHost(hostname, port, scheme)));
 
@@ -131,6 +158,25 @@ class ArticlesIndexerTestInteg {
         BulkRequest request = indexer.getBulkRequest(index, failedArticles);
 
         assertTrue(indexer.indexArticles(failedArticles, request));
-        // TODO: HTTP REQUEST TO SEE NUMBER OF FILES AND FAILURE
+
+        int hits = getHits();
+
+        assertEquals(failedArticles.size() - 1, hits);
+
+        clearIndex();
+    }
+
+    private int getHits() throws UnirestException, JsonProcessingException {
+        HttpResponse<String> response = Unirest.post(searchURL)
+                .header("Content-Type", "application/json")
+                .body("{\r\n    \"query\": {\r\n        \"match_all\": {}\r\n    }\r\n}")
+                .asString();
+
+        JsonNode jsonResponse = mapper.readTree(response.getBody());
+        return Integer.parseInt(jsonResponse.findValue("hits").findValue("total").findValue("value").toString());
+    }
+
+    private void clearIndex() {
+
     }
 }
